60ef57dadfabb1e8f9d5e4d02f9c8f4595a9cad0
YatheesanC <>
2020-07-10 18:13:18 +0530

    basic functions

--- distributed-node/src/main/java/com/msc/node/distributednode/Node.java
+++ distributed-node/src/main/java/com/msc/node/distributednode/Node.java
@@ -1,6 +1,9 @@
 package com.msc.node.distributednode;
 
 import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.util.List;
 import java.util.logging.Logger;
 
 public class Node {
@@ -15,7 +18,26 @@ public class Node {
 
     public Node(String userName) throws Exception{
         socket = new DatagramSocket();
+        socket.connect(InetAddress.getByName("8.8.8.8"),10002);
         this.ipAddress = socket.getLocalAddress().getHostAddress();
         LOG.info(this.ipAddress);
+        this.userName = userName;
+    }
+
+    private List<InetSocketAddress> register(){
+        List<InetSocketAddress> targets = null;
+        return targets;
+    }
+
+    public void unRegister() {
+        // method to unregister from network
+    }
+
+    public void joinOtherNodes(){
+        // method to inform otehr nodes based on BS given ip address of other nodes
+    }
+
+    public void printRoutingTable(){
+        // print the routing table
     }
 }

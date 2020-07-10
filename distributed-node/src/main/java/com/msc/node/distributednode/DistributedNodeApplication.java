3ebc6b4cb4ed34df7c20e106af50e3816726beda
YatheesanC <>
2020-07-10 19:38:48 +0530

    added uuid to get a unique name for the each nodes

--- distributed-node/src/main/java/com/msc/node/distributednode/DistributedNodeApplication.java
+++ distributed-node/src/main/java/com/msc/node/distributednode/DistributedNodeApplication.java
@@ -3,13 +3,16 @@ package com.msc.node.distributednode;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.UUID;
+
 @SpringBootApplication
 public class DistributedNodeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DistributedNodeApplication.class, args);
 		try {
-			Node node = new Node("node");
+			String uniqueID = UUID.randomUUID().toString();
+			Node node = new Node("node" + uniqueID);
 			node.register();
 			while (true){
 
